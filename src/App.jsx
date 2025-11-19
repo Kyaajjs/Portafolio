47e32a16b9f1b447943a4da1bc007e4306ce0a17
Ulises Zarate <al066575@uacam.mx>
2025-11-19 01:34:54 -0600

    Corregir rutas de imágenes

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ const projects = [
     title: "Escapa de SoyPan",
     tags: ["JavaScript", "Canvas", "Juego"],
     description: "Arcade 2D hecho con JavaScript puro.",
-    img: "./img/dos.png",  // <- CORREGIDO
+    img: "assets/img/dos.png",  // <- CORREGIDO
     repo: "https://github.com/Kyaajjs/EscapaDeSoyPan",
     live: "https://kyaajjs.github.io/EscapaDeSoyPan/",
   },
@@ -245,4 +245,4 @@ export default function App() {
       </footer>
     </div>
   );
-}
+} 
